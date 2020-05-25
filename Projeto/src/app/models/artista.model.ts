f1582a3aa44c3626088aaf77cec46b43e3060f07
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2020-05-24 23:59:11 -0300

    interface Artista e Evento

---
+++ Projeto/src/app/models/artista.model.ts
@@ -0,0 +1,10 @@
+export interface artista {
+    id: number;
+    urlImg: String;
+    nome: string;
+    tipo: string;
+    descricao: string;
+    dataDeNascimento: string;
+    email: String;
+    telefone: number;
+}
---
+++ Projeto/src/app/models/evento.model.ts
@@ -0,0 +1,19 @@
+export interface Evento {
+    id: number;
+    idArtistas: number[];
+    urlImg: String;
+    nome: string;
+    valor: string;
+    tipo: string;
+    descricao: string;
+    data: string;
+    horario: string;
+    contatoEvento: string;
+    endereco: {
+        cidade: string;
+        bairro: string;
+        rua: string;
+        numero: number;
+        complemento: string;
+    };
+}
