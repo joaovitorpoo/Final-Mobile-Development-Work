06f34b46f2ad49f49af03976c159090ef809cd61
samaraduarte <51268179+samaraduarte@users.noreply.github.com>
2020-06-03 15:08:29 -0300

    Interface componente cadastro

--- Projeto/src/app/cadastro/cadastro.page.ts
+++ Projeto/src/app/cadastro/cadastro.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { artista } from '../models/artista.model';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+
+  artista = {} as artista;
 
   constructor() { }
 
